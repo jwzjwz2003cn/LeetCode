5013859df3f4d1590550ff83124edd923d1fea94
Frank.Ji <fwji@outlook.com>
2014-11-13 14:04:10 -0500

    working add binary

---
+++ LeetCodeProblems/src/leetcode/solutions/String2Integer.java
@@ -0,0 +1,11 @@
+package leetcode.solutions;
+
+public class String2Integer implements ISolution {
+
+	@Override
+	public void test() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- LeetCodeProblems/src/leetcode/solutions/TestClass.java
+++ LeetCodeProblems/src/leetcode/solutions/TestClass.java
@@ -4,7 +4,7 @@ public class TestClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		ValidPalindrome solution = new ValidPalindrome();
+		AddBinary solution = new AddBinary();
 		solution.test();
 	}
 
